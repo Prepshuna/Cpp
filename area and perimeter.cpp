d9f2d6073182115cc7294a462eea7d1f5ab296ed
Prepshu <prepshunadhakal@gmail.com>
2025-06-15 18:42:08 +0545

    Add files via upload

---
+++ area and perimeter.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class Calc{
+	public:
+	int l,b;
+	int area(int l,int b)
+	{
+		return l*b;
+	}
+	int perimeter(int l,int b)
+	{
+		return (2*(l+b));
+	}
+};
+int main()
+{
+	Calc rectangle;
+	int l, b;
+	cout<<"Enter length and breadth: ";
+	cin>>l>>b;
+	cout<<"Area of rectangle is: "<<rectangle.area(l,b)<<endl;
+	cout<<"Perimeter of rectangle is: "<<rectangle.perimeter(l,b);
+	return 0;
+}
